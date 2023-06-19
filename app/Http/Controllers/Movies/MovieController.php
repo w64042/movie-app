b23007b5214a0d810155a298ed6c90b65cb70b63
witoldandreasik <witold.andreasik@xeler.it>
2023-06-19 16:03:09 +0200

    add movie and series resource

--- app/Http/Controllers/Movies/MovieController.php
+++ app/Http/Controllers/Movies/MovieController.php
@@ -11,7 +11,6 @@ class MovieController extends Controller
 
     public function index()
     {
-        return response()->json(['message' => 'Hello World']);
     }
 
     public function store(Request $request)
--- app/Http/Controllers/Series/SeriesController.php
+++ app/Http/Controllers/Series/SeriesController.php
@@ -10,7 +10,7 @@ class SeriesController extends Controller
 
     public function index()
     {
-        return response()->json(['message' => 'Hello World']);
+
     }
 
     public function store(Request $request)
