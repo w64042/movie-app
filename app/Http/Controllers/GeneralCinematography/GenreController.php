3241705da9fb266d6090b285b398298d5dbc1c3e
witoldandreasik <witold.andreasik@xeler.it>
2023-06-24 10:13:44 +0200

    add logic to GenreController

--- app/Http/Controllers/GeneralCinematography/GenreController.php
+++ app/Http/Controllers/GeneralCinematography/GenreController.php
@@ -7,5 +7,52 @@ use Illuminate\Http\Request;
 
 class GenreController extends Controller
 {
-    //
+    public function index()
+    {
+        $genres = Genre::all();
+
+        return response()->json($genres);
+    }
+
+    public function show($id)
+    {
+        $genre = Genre::findOrFail($id);
+
+        return response()->json($genre);
+    }
+
+    public function store(Request $request)
+    {
+
+        $request->validate([
+            'name' => 'required|string'
+        ]);
+
+        $genre = Genre::create([
+            'name' => $request->get('name')
+        ]);
+
+        return response()->json($genre);
+    }
+
+    public function update(Request $request, Genre $genre)
+    {
+        $request->validate([
+            'name' => 'string'
+        ]);
+
+        $genre->update([
+            'name' => $request->get('name')
+        ]);
+
+        return response()->json($genre);
+    }
+
+    public function destroy(Genre $genre)
+    {
+        $genre->delete();
+
+        return response()->json(['message' => 'Genre deleted']);
+    }
+
 }
